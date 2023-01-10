708fa789562c64accaee33f9eebf2da2d8f30edb
Cloud <kaiweicai@163.com>
2023-01-10 19:09:40 +0800

    add order trait to PriceOrder

--- src/grab_handicap.rs
+++ src/grab_handicap.rs
@@ -2,14 +2,14 @@
 // 比如现在卖1是60买1是70, 此策略会以65为中界，65以下布满买单，65以上布满卖单, 因为需要不停的调整订单布局，暂起名为高频逼近型
 // 注意: 模拟测试GetTicker的买一卖一固定差价为1.6, 实际效果需要实盘测试
 
-use std::{collections::HashMap, iter::Map};
+use std::{collections::{HashMap, BTreeMap}, iter::Map, fmt};
 
 use bian_rs::{
     client::UFuturesWSClient,
     error::BianResult,
     response::{self, WebsocketResponse, DepthOrder, WSFuturesDepth},
 };
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer, de::{Visitor, SeqAccess, Unexpected}};
 
 use crate::client::init_client;
 
@@ -98,8 +98,93 @@ pub struct WSFuturesOrderBook {
     pub pu: usize,
     /// 买方
     #[serde(rename = "b")]
-    pub buy: HashMap<f64,DepthOrder>,
+    pub buy: BTreeMap<PriceOrder,DepthOrder>,
     /// 卖方
     #[serde(rename = "a")]
-    pub sell: HashMap<f64,DepthOrder>,
+    pub sell: BTreeMap<PriceOrder,DepthOrder>,
+}
+
+
+#[derive(Debug)]
+pub struct PriceOrder(f64);
+
+impl Ord for PriceOrder{
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        return self.0.cmp(other.0)
+    }
+
+    fn max(self, other: Self) -> Self
+    where
+        Self: Sized,
+        Self: ~const std::marker::Destruct,
+    {
+        // HACK(fee1-dead): go back to using `self.max_by(other, Ord::cmp)`
+        // when trait methods are allowed to be used when a const closure is
+        // expected.
+        match self.cmp(&other) {
+            std::cmp::Ordering::Less | std::cmp::Ordering::Equal => other,
+            std::cmp::Ordering::Greater => self,
+        }
+    }
+
+    fn min(self, other: Self) -> Self
+    where
+        Self: Sized,
+        Self: ~const std::marker::Destruct,
+    {
+        // HACK(fee1-dead): go back to using `self.min_by(other, Ord::cmp)`
+        // when trait methods are allowed to be used when a const closure is
+        // expected.
+        match self.cmp(&other) {
+            std::cmp::Ordering::Less | std::cmp::Ordering::Equal => self,
+            std::cmp::Ordering::Greater => other,
+        }
+    }
+
+    fn clamp(self, min: Self, max: Self) -> Self
+    where
+        Self: Sized,
+        Self: ~const std::marker::Destruct,
+        Self: ~const PartialOrd,
+    {
+        assert!(min <= max);
+        if self < std::cmp::min {
+            std::cmp::min
+        } else if self > std::cmp::max {
+            std::cmp::max
+        } else {
+            self
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for PriceOrder {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_tuple(1, PriceOrderVisitor)
+    }
+}
+
+struct PriceOrderVisitor;
+
+impl<'de> Visitor<'de> for PriceOrderVisitor {
+    type Value = PriceOrder;
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a tuple of (String)")
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: SeqAccess<'de>,
+    {
+        let first: &'de str = seq
+            .next_element()?
+            .ok_or_else(|| serde::de::Error::invalid_value(Unexpected::Option, &"first element"))?;
+        let first_val = first.parse::<f64>().map_err(|_| {
+            serde::de::Error::invalid_value(Unexpected::Str(first), &"first element")
+        })?;
+        Ok(PriceOrder(first_val))
+    }
 }
